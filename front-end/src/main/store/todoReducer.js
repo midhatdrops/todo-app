d2c822eb80bfd5c8ef42ce4e5656242179ea49bb
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-18 10:05:40 -0300

    :art: Redux implements
    Configured Redux with Todo Component. Changed ToDo to class component and used life cycle (will mount). Used Redux Promise to resolve axios async problem with Redux.

--- front-end/src/main/store/todoReducer.js
+++ front-end/src/main/store/todoReducer.js
@@ -1,16 +1,15 @@
 const INITIAL_STATE = {
-  description: 'ler Livro',
-  list: [
-    { _id: 1, description: 'Pagar fatura do cartão', done: true },
-    { _id: 2, description: 'Ler Harry Potter', done: false },
-    { _id: 3, description: 'Reunião com a equipe as 10h', done: false },
-  ],
+  description: '',
+  list: [],
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'DESCRIPTION_CHANGED': {
       return { ...state, description: action.payload };
+    }
+    case 'TODO_SEARCH': {
+      return { ...state, list: action.payload.data };
     }
     default:
       return state;
--- front-end/src/todo/todoForm.jsx
+++ front-end/src/todo/todoForm.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Grid from '../template/grid.jsx'
 import IconButton from '../template/iconButton.jsx'
 
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import {changeDescription} from '../main/actions/todoActions.js'
+import {changeDescription,search} from '../main/actions/todoActions.js'
 
-const todoForm =  props => {
-  const keyHandler = (e) => {
+class todoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.keyHandler = this.keyHandler.bind(this)
+  }
+
+  componentWillMount() {
+    this.props.search()
+}
+
+  keyHandler(e) {
     if(e.key === 'Enter') {
-      e.shiftKey ? props.handleSearch() : props.handleAdd()
+      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
     } else if (e.key === 'Escape') {
-      props.handleClear()
+      this.props.handleClear()
     }
   }
-  return ( 
-  <div role="form" className="todoForm">
-  <Grid cols='12 9 10'>
-    <input id="description" className='form-control' placeholder="Adicione uma tarefa" value={props.description} onChange={props.changeDescription} onKeyUp={keyHandler}></input>
+
+  render() {
+    return (<div role="form" className="todoForm">
+    <Grid cols='12 9 10'>
+      <input id="description" className='form-control' placeholder="Adicione uma tarefa" value={this.props.description} onChange={this.props.changeDescription} onKeyUp={this.keyHandler}></input>
+      </Grid>
+  
+    <Grid cols='12 3 2'>
+      <IconButton style="primary" icon="plus" onClick={this.props.handleAdd}/>
+      <IconButton style='info' icon="search" onClick={this.props.handleSearch}/>
+      <IconButton style='default' icon="close" onClick={this.props.handleClear}/>
     </Grid>
-
-  <Grid cols='12 3 2'>
-    <IconButton style="primary" icon="plus" onClick={props.handleAdd}/>
-    <IconButton style='info' icon="search" onClick={props.handleSearch}/>
-    <IconButton style='default' icon="close" onClick={props.handleClear}/>
-  </Grid>
-</div>)
+  </div>)
+  }
 }
 
-const mapStateToProps = state => ({
-  description: state.todo.description
-})
 
-const mapDispatchToProps = dispatch => {
-  bindActionCreators({
-    changeDescription
-  }, dispatch)
-}
+
+const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => 
+    bindActionCreators({changeDescription, search}, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(todoForm)
